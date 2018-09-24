8a14b772416c7f1b95276e37c3094c9bb8ddf07d
GianniFournier <43540865+GianniFournier@users.noreply.github.com>
2018-09-24 16:37:02 +0200

    Demo script add

--- Assets/scripts/CharacterControllerBehaviour.cs
+++ Assets/scripts/CharacterControllerBehaviour.cs
@@ -1,13 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
+
 
 public class CharacterControllerBehaviour : MonoBehaviour {
 
-	// Use this for initialization
+
+    private CharacterController _characterController;
+
 	void Start () {
-		
-	}
+
+        _characterController = GetComponent<CharacterController>();
+
+
+#if DEBUG
+
+        if (_characterController == null)
+            Debug.Log("ERROR: _characterController needs a component! (is null)");
+
+        Assert.IsNotNull(_characterController, "ERROR: _characterController needs a component! (is null)");
+
+#endif
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
